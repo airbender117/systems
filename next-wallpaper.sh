773e920d091b6b255f4acf1a105adfdcc8aa9671
airbender117 <anuragshrestha117@gmail.com>
2025-12-26 15:57:28 +0545

    feh

---
+++ next-wallpaper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# next-wallpaper.sh
+
+WALLPAPER_DIR="$HOME/wall/"  # folder with wallpapers
+
+# pick a random wallpaper
+FILE=$(ls "$WALLPAPER_DIR" | shuf -n1)
+feh --bg-scale "$WALLPAPER_DIR/$FILE"
+
